44f454842ac0c8e364111b3aa2fc342a2d27cc58
estel1591 <estel1591@mail.ru>
2018-09-29 11:01:35 +0300

    update index.js (correct function check)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [];
+
+    for (let i = 0, length = bracketsConfig.length; i < length; i++ ) {
+        brackets.push(bracketsConfig[i].join(''));
+    }
+    
+    for (let i = 0, length = brackets.length; i < length; i++ ) {
+        let delPosition = str.indexOf(brackets[i]);
+        if (delPosition > -1) {
+            str =  str.slice(0, delPosition) + str.slice(delPosition+2);
+            i = -1;
+            delPosition = -1;
+        }
+    }  
+    return str.length? false : true;
+
 }
+
